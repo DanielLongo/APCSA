3e294ceca2a8f2c6228c87ec08487119696aa9ab
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2018-10-03 10:23:33 -0700

    comments added

--- Projects/Project2 - TheBall/BallRunner.java
+++ Projects/Project2 - TheBall/BallRunner.java
@@ -84,7 +84,7 @@ public class BallRunner {
         }
          for (int i = 0; i < findFreeBallBotIndex(ballBots); i++) {
              currBallBot = ballBots[i];
-             if(currBallBot.canMoveForward(ballWorld) && (ballBotToBounceOff(currBallBot, ballBots) == null)) currBallBot.moveForward();
+             if(currBallBot.canMoveForward(ballWorld) && (ballBotToBounceOff(currBallBot, ballBots) == true)) currBallBot.moveForward();
              else {
                  currBallBot.setHeading((currBallBot.getHeading() + 110) % 360);
                 }
@@ -100,64 +100,57 @@ public static void a5() {
      int curRadius;
      BallBot currBallBot;
      for (int i = 0; i < ballBots.length; i ++) {
-        ballBotRadiuses[i] = (int)(Math.random() * 50);
+        ballBotRadiuses[i] = (int)(Math.random() * 50); //creates array of randome radii
     }
      while (true) {
+         //creates new ballBots
          if (findFreeBallBotIndex(ballBots) != ballBots.length) {
              curRadius = ballBotRadiuses[findFreeBallBotIndex(ballBots)];
-             if (findFreeBallBotIndex(ballBots) == 0) {
+             if (findFreeBallBotIndex(ballBots) == 0) { //the first ball bot will always have a clear entrance
              ballBots[findFreeBallBotIndex(ballBots)] = new BallBot(ballWorld, entrancePoint, Math.random() * 360.,curRadius);
                 }
              else if (entranceClearByRadius(ballBots[findFreeBallBotIndex(ballBots)-1], curRadius, entrancePoint)) {
+                 //if the enough room in entrance creates new bot
                  ballBots[findFreeBallBotIndex(ballBots)] = new BallBot(ballWorld, entrancePoint, Math.random() * 360.,curRadius);
          }
         }
+        
+        //moves ballBots
          for (int i = 0; i < findFreeBallBotIndex(ballBots); i++) {
              currBallBot = ballBots[i];
-             if(currBallBot.canMoveForward(ballWorld) && (ballBotToBounceOff(currBallBot, ballBots) == null)) {
+             if(currBallBot.canMoveForward(ballWorld) && (ballBotToBounceOff(currBallBot, ballBots) == false)) {
                  currBallBot.moveForward();
-                 currBallBot.setPixelsPerSecond(currBallBot.getPixelsPerSecond() + 2);
+                 currBallBot.setPixelsPerSecond(currBallBot.getPixelsPerSecond() + 4); //accelerates ballBot
                 }
              else {
-                 currBallBot.setHeading((currBallBot.getHeading() + 110) % 360);
-                 currBallBot.setPixelsPerSecond(1);
-                 currBallBot.setColor(currBallBot.getColor() + 1);
+                 currBallBot.setHeading((currBallBot.getHeading() + 110) % 360); //changes heading on colision
+                 currBallBot.setPixelsPerSecond(1); //slows down ball bot on collision
+                 currBallBot.setColor(currBallBot.getColor() + 1); //changes color on collision
                 }
          }
         }
 }
 public static int findFreeBallBotIndex(BallBot[] ballBotArray) {
     for (int i = 0; i < ballBotArray.length; i++) {
+        //retuns index next unitialized ball bot
         if (ballBotArray[i] == null) return i;
     }
     return ballBotArray.length;
 }
 public static double distanceBetweenPoints (TGPoint point1, TGPoint point2) {
+    //distance formula implemented 
     return Math.pow((Math.pow((point1.xDoubleValue() - point2.xDoubleValue()), 2) + Math.pow((point1.yDoubleValue() - point2.yDoubleValue()), 2)), .5);
 }
 
 public static boolean entranceClearByRadius(BallBot ballBot, int radiusNewBot, TGPoint entrancePoint) {
     double distance;
     distance = distanceBetweenPoints(ballBot.getPoint(), entrancePoint);
+    //ensures distance is greater than the radii of the two bots 
     if (distance > (ballBot.getRadius() + radiusNewBot)) return true;
     return false;
 }
-public static boolean entranceClear (BallBot[] ballBotArray, TGPoint entrancePoint) {
-   for (int i = 0; i < ballBotArray.length; i++) {
-       if (ballBotArray[i] == null) {
-        if (i == 0) {
-            return true;
-        }
-        else if (distanceBetweenPoints(ballBotArray[i-1].getPoint(), ballBotArray[i].getPoint()) >= (ballBotArray[i-1].getRadius() + ballBotArray[i].getRadius())){
-           return true;
-        }
-        else return false;
-       }
-    }
-   return false;
-}
 
-public static BallBot ballBotToBounceOff(BallBot ballBot, BallBot[] ballBotArray) {
+public static boolean ballBotToBounceOff(BallBot ballBot, BallBot[] ballBotArray) {
     TGPoint currPoint = ballBot.getPoint();
     TGPoint nextPoint = ballBot.forwardPoint();
     BallBot currBallBot;
@@ -165,16 +158,16 @@ public static BallBot ballBotToBounceOff(BallBot ballBot, BallBot[] ballBotArray
     double nextDistance;
     for (int i = 0;i < ballBotArray.length;i++) {
         currBallBot = ballBotArray[i];
-        if ((currBallBot != null) && (currBallBot != ballBot)) {
+        if ((currBallBot != null) && (currBallBot != ballBot)) { //checks if there are two unique ball bots
             currDistance = distanceBetweenPoints(currBallBot.getPoint(), currPoint);
             if (currDistance <= currBallBot.getRadius() + ballBot.getRadius()) {
                 nextDistance = distanceBetweenPoints(nextPoint,currBallBot.getPoint());
             if (nextDistance <= currDistance) {
-                return currBallBot;
+                return true; //returns true if bots will colide
             }
         }
         }
     }
-    return null;
+    return false; //returns false bots will not colide
 }    
 }
