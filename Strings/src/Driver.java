ef9e8977433d460dbe55897ea06d6102d50e3cb8
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2018-10-05 09:56:40 -0700

    file structure updated

--- Strings/src/Driver.java
+++
@@ -1,8 +0,0 @@
-public class Driver {
-    public static void main(String[] args) {
-        CreateSentence x = new CreateSentence();
-        System.out.println(x.getSentence());
-        System.out.println(x.getSentence());
-        System.out.println(x.getSentence());
-    }
-}
