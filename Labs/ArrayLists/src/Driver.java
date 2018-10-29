b6d52bbcca3ad2409b61ab6d6bd20e0fb2e40155
Daniel Dominic Longo <danieldominiclongo@gmail.com>
2018-10-29 10:30:51 -0700

    updates to array list

--- Labs/ArrayLists/src/Driver.java
+++ Labs/ArrayLists/src/Driver.java
@@ -1,9 +1,49 @@
+import java.util.Scanner;
 public class Driver {
+    public static Scanner scr;
     public static void main(String[] args) {
-        Student s1 = new Student("Daniel", "Dominic", "Longo", 798213, 0.0);
-        StudentList stuList = new StudentList();
-        stuList.addStudent (s1);
-        stuList.printStudent (0);
+        int input = 1;
+        scr = new Scanner (System.in);
+        StudentList stuList = new StudentList ( );
+        while (input > 0) {
+            input = menu ();
+            switch (input) {
+                case 1:
+                    System.out.println ("Print a student");
+                    stuList.printStudent (-1);
+                    break;
+                case 2:
+                    System.out.println ("Add a new student");
+                    stuList.createStudent ();
+                    break;
+            }
+//            Student s1 = new Student ("Daniel", "Dominic", "Longo", 798213, 0.0);
+//            stuList.addStudent (s1);
+//            stuList.printStudent (0);
+//            stuList.createStudent ( );
+//            System.out.println ("Finished");
 
+        }
+    }
+    public static int menu() {
+        System.out.println ("To quit: q");
+        System.out.println ("To print a student: 1");
+        System.out.println ("To add a student: 2");
+        System.out.println ("To remove a student: 3");
+        System.out.println ("To modify a student: 4");
+        System.out.print ("Enter: ");
+        String response = scr.nextLine ( );
+        int numericResponse;
+        try {
+            numericResponse = Integer.parseInt (response);
+        }
+        catch (NumberFormatException e) {
+            if (response.equals (new String ("q"))) return 0;
+            System.out.println ("Invalid Response.");
+            return menu();
+
+        }
+        if ((numericResponse > 0) && (numericResponse < 4)) return numericResponse;
+        return menu();
     }
 }
--- Labs/ArrayLists/src/StudentList.java
+++ Labs/ArrayLists/src/StudentList.java
@@ -1,13 +1,54 @@
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class StudentList {
     ArrayList<Student> students;
+    Scanner scr;
     public StudentList() {
+        scr = new Scanner (System.in);
         students = new ArrayList<Student>();
     }
 
     public void addStudent(Student newStudent) {
         students.add(newStudent);
     }
+
+    public String[] parseName(String fullName) {
+        String[] nameArray = new String[3];
+        if (fullName.split(",").length == 3) {
+            return fullName.split(",");
+        }
+        System.out.println ("Invalid Name entered. Please use FIRST, MIDDLE, LAST");
+        return new String[]{"", "", ""};
+
+    }
+    public void createStudent() {
+        System.out.print ("Enter Student Name: ");
+        String name = scr.nextLine ( );
+        String[] nameArray = new String[3];
+        nameArray = parseName (name);
+        if (nameArray[0].equals (new String (""))) {
+            System.out.println ("Invalid Credentials. Please Restart");
+            createStudent ( );
+        }
+        String firstName = nameArray[0];
+        String middleName = nameArray[1];
+        String lastName = nameArray[2];
+
+        try {
+            System.out.print ("Enter Student Number: ");
+            int studentNumber = Integer.parseInt (scr.nextLine ( ).replaceAll ("\\s+", ""));
+            System.out.print ("Enter Student GPA: ");
+            double studentGPA = Double.parseDouble ((scr.nextLine ( )).replaceAll ("\\s+", ""));
+            Student newStudent = new Student (firstName, middleName, lastName, studentNumber, studentGPA);
+            addStudent (newStudent);
+        }
+        catch (NumberFormatException e) {
+            System.out.println("Student Number and GPA must be numbers. Please Restart");
+            createStudent ( );
+        }
+    }
+
 
     public void removeStudent(int index) {
         if (index == -1) {
@@ -21,13 +62,20 @@ public class StudentList {
     }
 
     public void printStudent(int index) {
+        if (index == -1) {
+            index = students.size () -1;
+        }
+
+        else if (index == -2) {
+            System.out.print ("h");
+        }
         Student student = students.get(index);
         System.out.println(
-                "First " + student.getFirstName ()  + "\n" +
-                "Middle " + student.getLastName () + "\n" +
-                        "Last " + student.getLastName()
-
-
+                "First: " + student.getFirstName ()  + "\n" +
+                "Middle: " + student.getLastName () + "\n" +
+                        "Last: " + student.getLastName() + "\n" +
+                        "Student Id: " + student.getStuNumber () + "\n" +
+                        "Student GPA: " + student.getGpa () + "\n"
         );
     }
 }
